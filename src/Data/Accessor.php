84bd8e8831dc48fb0f3c80bdbc88af65be2feb58
Asao <kamei@workspot.jp>
2015-11-18 20:21:44 +0900

    arranged code trying to streamline the logic.

--- src/Data/Accessor.php
+++ src/Data/Accessor.php
@@ -13,23 +13,22 @@ class Accessor
      */
     public static function get($data, $key, $default = null)
     {
-        if ((is_array($data) || $data instanceof \ArrayAccess)
-            && array_key_exists($key, $data)
-        ) {
-            return $data[$key];
+        if ((is_array($data) || $data instanceof \ArrayAccess)) {
+            return array_key_exists($key, $data) ? $data[$key]: $default;
         }
-        if (is_object($data)) {
-            if (isset($data->$key)) {
-                return $data->$key;
-            }
-            $method = 'get' . $key;
-            if (method_exists($data, $method)) {
-                return $data->$method();
-            }
-            $method = 'get' . str_replace('_', '', $key);
-            if (method_exists($data, $method)) {
-                return $data->$method();
-            }
+        if (!is_object($data)) {
+            return $default;
+        }
+        $method = 'get' . $key;
+        if (method_exists($data, $method)) {
+            return $data->$method();
+        }
+        $method = 'get' . str_replace('_', '', $key);
+        if (method_exists($data, $method)) {
+            return $data->$method();
+        }
+        if (isset($data->$key)) {
+            return $data->$key;
         }
 
         return $default;
@@ -45,22 +44,20 @@ class Accessor
      */
     public static function has($data, $key)
     {
-        if (is_array($data)) {
+        if ((is_array($data) || $data instanceof \ArrayAccess)) {
             return array_key_exists($key, $data);
         }
-        if (is_object($data)) {
-            if ($data instanceof \ArrayAccess) {
-                return array_key_exists($key, $data);
-            }
-            if (isset($data->$key)) {
-                return true;
-            }
-            if (method_exists($data, 'get' . $key)) {
-                return true;
-            }
-            if (method_exists($data, 'get' . str_replace('_', '', $key))) {
-                return true;
-            }
+        if (!is_object($data)) {
+            return false;
+        }
+        if (method_exists($data, 'get' . $key)) {
+            return true;
+        }
+        if (method_exists($data, 'get' . str_replace('_', '', $key))) {
+            return true;
+        }
+        if (isset($data->$key)) {
+            return true;
         }
 
         return false;
