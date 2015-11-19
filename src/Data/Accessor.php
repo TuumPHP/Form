f5989f9722765a9d90cc32c439c9b96ff7c0019d
Asao <kamei@workspot.jp>
2015-11-19 22:21:15 +0900

    split the method into two part to reduce complexity.

--- src/Data/Accessor.php
+++ src/Data/Accessor.php
@@ -1,6 +1,8 @@
 <?php
 namespace Tuum\Form\Data;
 
+use ArrayAccess;
+
 class Accessor
 {
     /**
@@ -13,12 +15,35 @@ class Accessor
      */
     public static function get($data, $key, $default = null)
     {
-        if ((is_array($data) || $data instanceof \ArrayAccess)) {
-            return array_key_exists($key, $data) ? $data[$key]: $default;
+        if ((is_array($data) || $data instanceof ArrayAccess)) {
+            return self::getArray($data, $key, $default);
         }
-        if (!is_object($data)) {
-            return $default;
+        if (is_object($data)) {
+            return self::getObj($data, $key, $default);
         }
+
+        return $default;
+    }
+
+    /**
+     * @param array|ArrayAccess $data
+     * @param string            $key
+     * @param null              $default
+     * @return mixed
+     */
+    public static function getArray($data, $key, $default = null)
+    {
+        return array_key_exists($key, $data) ? $data[$key]: $default;
+    }
+
+    /**
+     * @param object $data
+     * @param string $key
+     * @param null   $default
+     * @return mixed
+     */
+    public static function getObj($data, $key, $default = null)
+    {
         $method = 'get' . $key;
         if (method_exists($data, $method)) {
             return $data->$method();
@@ -44,7 +69,7 @@ class Accessor
      */
     public static function has($data, $key)
     {
-        if ((is_array($data) || $data instanceof \ArrayAccess)) {
+        if ((is_array($data) || $data instanceof ArrayAccess)) {
             return array_key_exists($key, $data);
         }
         if (!is_object($data)) {
